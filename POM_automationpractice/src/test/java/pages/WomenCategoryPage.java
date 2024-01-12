a2dcc4df7b041972cb01a353662248baee2a39c2
Natalia Kiełbasa <kielbasa.natalia97@gmail.com>
2024-01-12 10:48:17 +0100

    basketprocessv1

--- POM_automationpractice/src/test/java/pages/WomenCategoryPage.java
+++ POM_automationpractice/src/test/java/pages/WomenCategoryPage.java
@@ -11,8 +11,20 @@ public class WomenCategoryPage extends BasePage {
         super();
     }
 
+    @FindBy(className = "ajax_add_to_cart_button")
+    private WebElement addToCartButton;
+
+    @FindBy(id = "layer_cart")
+    private WebElement addToCartPopup;
+
     @Step
     public void checkingOpeningWomenCategory() {
         Assertions.assertTrue(driver.getCurrentUrl().contains("id_category=3"), "Incorrect URL");
+    }
+
+    @Step
+    public void addToCart (){
+        addToCartButton.click();
+        Assertions.assertTrue(addToCartPopup.isDisplayed(), "The pop up is not displayed");
     }
 }
